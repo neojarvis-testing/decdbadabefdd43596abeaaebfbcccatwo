d7c5734f86444a50a94e9600f2f32dac544b64d5
neojarvis <techadmin@iamneo.ai>
2024-01-09 12:58:37 +0000

    Manual-save-commit-1/9/2024--6:28:34-PM

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 8081;
-
-//app.use('/static', express.static('public'))
-app.use(express.static('public'))
-app.listen(port, () => console.log(`listening on port ${port}!`));
